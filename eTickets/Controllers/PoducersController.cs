ab440ad3a2404e83211c57179888bbbb12d4d173
RadovanCW <radovan.parovic@companywall.rs>
2022-08-17 16:10:00 +0200

    Controllers

---
+++ eTickets/Controllers/PoducersController.cs
@@ -0,0 +1,26 @@
+﻿using eTickets.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eTickets.Controllers
+{
+    public class PoducersController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public PoducersController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var allProducers = await _context.Producers.ToListAsync();
+            return View();
+        }
+    }
+}
