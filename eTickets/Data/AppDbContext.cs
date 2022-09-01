b1d11f5dca46002074a216a3810e65a3fdb65b97
RadovanCW <radovan.parovic@companywall.rs>
2022-09-01 16:38:47 +0200

    Order

--- eTickets/Data/AppDbContext.cs
+++ eTickets/Data/AppDbContext.cs
@@ -36,5 +36,10 @@ namespace eTickets.Data
         public DbSet<Actor_Movie> Actors_Movies { get; set; }
         public DbSet<Cinema> Cinemas { get; set; }
         public DbSet<Producer> Producers { get; set; }
+
+        //Orders related table
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<ShoppingCartItem> shoppingCartItems { get; set; }
     }
 }
